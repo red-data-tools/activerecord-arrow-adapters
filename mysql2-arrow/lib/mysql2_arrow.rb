f7b66d93ed31aa5a111b4506c42f349e8ffcbb69
Kenta Murata <mrkn@mrkn.jp>
2019-03-24 09:36:15 +0900

    Add LoadError handling for mysql2_arrow.so

--- mysql2-arrow/lib/mysql2_arrow.rb
+++ mysql2-arrow/lib/mysql2_arrow.rb
@@ -1,6 +1,12 @@
 require "mysql2_arrow/version"
 require "mysql2"
 require "arrow"
-require "mysql2_arrow.so"
+
+begin
+  require "mysql2_arrow.so"
+rescue LoadError
+  $LOAD_PATH.unshift File.expand_path("../../ext/mysql2_arrow", __FILE__)
+  require "mysql2_arrow.so"
+end
 
 Mysql2::Result.include Mysql2Arrow::ResultExtension
