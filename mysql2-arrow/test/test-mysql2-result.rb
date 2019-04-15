42f2357686dbdf277fe111bf7f764eadb041e2dc
Kenta Murata <mrkn@mrkn.jp>
2019-04-16 00:08:27 +0900

    Fix mysql2-arrow test

--- mysql2-arrow/test/test-mysql2-result.rb
+++ mysql2-arrow/test/test-mysql2-result.rb
@@ -25,14 +25,13 @@ class Mysql2ResultTest < Test::Unit::TestCase
       database: "test"
     )
 
-    @result = @client.query(<<~SQL)
+    @query = <<~SQL
       SELECT
         tiny_int_test
         , small_int_test
         , medium_int_test
         , int_test
         , big_int_test
-        , float_test
         , double_test
         , decimal_test
         , varchar_test
@@ -43,38 +42,69 @@ class Mysql2ResultTest < Test::Unit::TestCase
         , long_text_test
       FROM mysql2_test LIMIT 30000
     SQL
+    @result = @client.query(@query)
   end
 
-  test("#to_arrow") do
-    table = @result.to_arrow
-    assert_kind_of(Arrow::Table,
-                   table)
-    assert_equal(30_000,
-                 table.n_rows)
-    assert_equal(14,
-                 table.n_columns)
-  end
+  sub_test_case("#to_arrow") do
+    test("schema") do
+      table = @result.to_arrow
+      assert_kind_of(Arrow::Table,
+                     table)
+      assert_equal(30_000,
+                   table.n_rows)
+      assert_equal(13,
+                   table.n_columns)
+      assert_equal(Arrow::Schema.new(
+                     tiny_int_test: :int8,
+                     small_int_test: :int16,
+                     medium_int_test: :int32,
+                     int_test: :int32,
+                     big_int_test: :int64,
+                     double_test: :double,
+                     decimal_test: {
+                                     type: :decimal128,
+                                     precision: 10,  # TODO
+                                     scale: 3
+                                   },
+                     varchar_test: :string,
+                     binary_test: :binary,
+                     tiny_text_test: :binary, # TODO
+                     text_test: :binary, # TODO
+                     medium_text_test: :binary, # TODO
+                     long_text_test: :binary # TODO
+                   ),
+                   table.schema)
+    end
 
-  test("#raw_records") do
-    query = <<~SQL
-      SELECT
-        tiny_int_test
-        , small_int_test
-        , medium_int_test
-        , int_test
-        , big_int_test
-        , double_test
-        , decimal_test
-        , varchar_test
-        , binary_test
-        , tiny_text_test
-        , text_test
-        , medium_text_test
-        , long_text_test
-      FROM mysql2_test LIMIT 10
-    SQL
+    test("content") do
+      query = @query.sub(/LIMIT \d+/, "LIMIT 10")
 
-    assert_equal(@client.query(query, as: :array).to_a,
-                 @client.query(query).to_arrow.raw_records)
+      table = @client.query(query).to_arrow
+
+      expected_records = @client.query(query, as: :array).to_a
+      expected_records.each do |record|
+        # TODO: red-arrow has bug of BigDecimal treatment
+        a, b = record[6].to_s('F').split('.', 2)
+        b = "#{b}000"[0, 3]
+        record[6] = [a, b].join('.')
+      end
+      record_batch = Arrow::RecordBatch.new(table.schema, expected_records)
+      expected_table = Arrow::Table.new(record_batch.schema, [record_batch])
+      assert_equal(expected_table, table)
+    end
+
+    test("raw_records") do
+      query = @query.sub(/LIMIT \d+/, "LIMIT 10")
+
+      table = @client.query(query).to_arrow
+      records = table.raw_records
+
+      expected_records = @client.query(query, as: :array).to_a
+      # assert_equal(expected_records, records)
+
+      expected_records.each_with_index do |record, i|
+        assert_equal([i, record], [i, records[i]])
+      end
+    end
   end
 end
